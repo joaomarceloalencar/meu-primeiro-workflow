2aaa645e78c28bcc4d657108e58fd471859cfc99
joaomarceloalencar <joao.marcelo@ufc.br>
2025-04-15 10:06:14 -0300

    Algumas sugestões de estilo.

--- app.py
+++ app.py
@@ -3,10 +3,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello_world():
-  """Retorna uma saudação simples."""
-  return 'Olá, Mundo DevOps com Flask e GitHub Actions!'
+    """Retorna uma saudação simples."""
+    return 'Olá, Mundo DevOps com Flask e GitHub Actions!'
 
 if __name__ == '__main__':
-  app.run(debug=True)
+    app.run(debug=True)
+
