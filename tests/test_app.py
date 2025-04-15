50ae1eb7e54d91b89be76c246df1c34324252873
joaomarceloalencar <joao.marcelo@ufc.br>
2025-04-15 10:08:29 -0300

    Resolvendo a sugestão de identação.

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,18 +2,20 @@
 import pytest
 from app import app as flask_app # Importa a instância da app Flask
 
+
 @pytest.fixture
 def app():
-  """Cria uma instância da app Flask para testes."""
-  yield flask_app
+    """Cria uma instância da app Flask para testes."""
+    yield flask_app
 
 @pytest.fixture
 def client(app):
-  """Cria um cliente de teste para a app Flask."""
-  return app.test_client()
+    """Cria um cliente de teste para a app Flask."""
+    return app.test_client()
 
 def test_hello_world(client):
-  """Testa se a rota principal ('/') retorna status 200 e contém a saudação."""
-  response = client.get('/')
-  assert response.status_code == 200
-  assert b'Ol\xc3\xa1, Mundo DevOps' in response.data # Usando bytes e codificação correta para 'Olá'
+    """Testa se a rota principal ('/') retorna status 200 e contém a saudação."""
+    response = client.get('/')
+    assert response.status_code == 200
+    assert b'Ol\xc3\xa1, Mundo DevOps' in response.data # Usando bytes e codificação correta para 'Olá'
+
