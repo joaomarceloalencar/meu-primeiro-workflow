7cb28e8edbe63597b9b161478f4dfdcdff9456cf
joaomarceloalencar <joao.marcelo@ufc.br>
2026-06-22 16:51:40 -0300

    Testes.

---
+++ tests/test_app.py
@@ -0,0 +1,18 @@
+import pytest
+from app import app as flask_app
+
+
+@pytest.fixture
+def app():
+    yield flask_app
+
+
+@pytest.fixture
+def client(app):
+    return app.test_client()
+
+
+def test_hello_world(client):
+    response = client.get('/')
+    assert response.status_code == 200
+    assert b'Ol\xc3\xa1, Mundo DevOps' in response.data
